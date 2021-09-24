eff33d78a8012e1c5f6f39638166f5c5235f9f97
Tolulope Adetula <abeltolu@gmail.com>
2021-09-24 12:58:22 +0100

    Remove Cart Item If Quantity Is Less Than 1

--- src/operations/mutations/cart/decrementQuantity.ts
+++ src/operations/mutations/cart/decrementQuantity.ts
@@ -2,14 +2,22 @@ import { ReactiveVar } from '@apollo/client';
 import { CartItems } from '../../../@types/Cart';
 import updateCartItem from './updateCartItem';
 import getCartItem from './getCartItem';
+import removeCartItem from './removeItem';
 
 export default function decrementQuantity(
     cartItemsVar: ReactiveVar<CartItems>
 ) {
     return (id: number) => {
         const cartItem = getCartItem(cartItemsVar)(id);
-        if (!cartItem || cartItem.quantity === 1) return;
+        if (!cartItem) return; //cart item does not exist
 
+        //if the quantity is 1 and the "-" button is pressed it should remove the item.
+        if (cartItem.quantity === 1) {
+            removeCartItem(cartItemsVar)(id);
+            return;
+        }
+
+        //update cart item quantity
         updateCartItem(cartItemsVar)({
             ...cartItem,
             quantity: cartItem.quantity - 1,
